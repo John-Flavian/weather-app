a77bf781799e7a299bf25a1790ec1f7543bfe5fb
John-Flavian <flavianomeokwe@gmail.com>
2023-07-04 06:34:22 +0100

    Feat: Adds the bottom tab navigation to the app.

---
+++ src/utilities/weatherType.js
@@ -0,0 +1,12 @@
+export const weatherType = {
+  Thunderstorm: {
+    icon: "zap",
+    message: "It could get noisy",
+    backgroundColor: "#000000"
+  },
+  Drizzle: {
+    icon: "cloud-rain",
+    message: "It might rain a little",
+    backgroundColor: "#000000"
+  }
+};
