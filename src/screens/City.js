d72744e2db22fad91fa330e9409aec593c0fef94
John-Flavian <flavianomeokwe@gmail.com>
2023-07-20 02:44:59 +0100

    feat: adds the fetched data to the City screen.

--- src/screens/City.js
+++ src/screens/City.js
@@ -5,11 +5,12 @@ import {
   StyleSheet,
   ImageBackground,
   View,
-  StatusBar
+  StatusBar,
 } from "react-native";
 import IconText from "../components/IconText";
+import moment from "moment";
 
-const City = () => {
+const City = ({ weatherData }) => {
   const {
     container,
     cityName,
@@ -20,8 +21,9 @@ const City = () => {
     riseSetText,
     riseSetWrapper,
     imageLayout,
-    rowLayout
+    rowLayout,
   } = styles;
+  const { name, country, population, sunrise, sunset } = weatherData;
   return (
     <SafeAreaView style={container}>
       <ImageBackground
@@ -29,14 +31,14 @@ const City = () => {
         source={require("../../assets/city-background.jpg")}
         style={imageLayout}
       >
-        <Text style={[cityName, cityText]}> Lagos </Text>
-        <Text style={[countryName, cityText]}>Nigeria</Text>
+        <Text style={[cityName, cityText]}> {name} </Text>
+        <Text style={[countryName, cityText]}>{country}</Text>
 
         <View style={[populationWrapper, rowLayout]}>
           <IconText
             iconName={"user"}
             iconColor={"red"}
-            bodyText={"8000"}
+            bodyText={`Population: ${population}`}
             bodyTextStyles={populationText}
           />
         </View>
@@ -45,14 +47,14 @@ const City = () => {
           <IconText
             iconName={"sunrise"}
             iconColor={"white"}
-            bodyText={"10:46:58am"}
+            bodyText={moment(sunrise).format("h:mm:ss a")}
             bodyTextStyles={riseSetText}
           />
 
           <IconText
             iconName={"sunset"}
             iconColor={"white"}
-            bodyText={"17:28:15pm"}
+            bodyText={moment(sunset).format("h:mm:ss a")}
             bodyTextStyles={riseSetText}
           />
         </View>
@@ -64,46 +66,46 @@ const City = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: StatusBar.currentHeight || 0
+    marginTop: StatusBar.currentHeight || 0,
   },
   imageLayout: {
-    flex: 1
+    flex: 1,
   },
   cityName: {
-    fontSize: 40
+    fontSize: 40,
   },
   countryName: {
-    fontSize: 30
+    fontSize: 30,
   },
   cityText: {
     justifyContent: "center",
     alignSelf: "center",
     fontWeight: "bold",
-    color: "white"
+    color: "white",
   },
   populationWrapper: {
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
-    marginTop: 30
+    marginTop: 30,
   },
   populationText: {
     fontSize: 25,
     marginLeft: 7.5,
-    color: "red"
+    color: "red",
   },
   riseSetWrapper: {
     justifyContent: "space-around",
-    marginTop: 30
+    marginTop: 30,
   },
   riseSetText: {
     fontSize: 20,
-    color: "white"
+    color: "white",
   },
   rowLayout: {
     flexDirection: "row",
-    alignItems: "center"
-  }
+    alignItems: "center",
+  },
 });
 
 export default City;
